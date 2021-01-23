765775e1675a225737f1e43dd91082be019382f4
Ayfri <pierre.ayfri@gmail.com>
2021-01-23 13:51:17 +0100

    Improved code for InlineAssetsPlugin.js.

--- InlineAssetsPlugin.js
+++ InlineAssetsPlugin.js
@@ -5,50 +5,32 @@ class InlineAssetsPlugin {
     }
 
     apply(compiler) {
-        compiler.hooks.emit.tapAsync(
-            "InlineAssetsPlugin",
-            (compilation, callback) => {
-                for (const pattern of this.patterns) {
-                    const fromSource = this.getSource(
-                        compilation.getAsset(pattern.from)
-                    );
-                    const toSource = this.getSource(
-                        compilation.getAsset(pattern.to)
-                    );
+        compiler.hooks.emit.tapAsync("InlineAssetsPlugin", (compilation, callback) => {
+            for (const pattern of this.patterns) {
+                const fromSource = this.getSource(compilation.getAsset(pattern.from));
+                const toSource = this.getSource(compilation.getAsset(pattern.to));
 
-                    const resultSource = toSource.replace(
-                        pattern.pattern,
-                        fromSource
-                    );
+                const resultSource = toSource.replace(pattern.pattern, fromSource);
 
-                    compilation.updateAsset(pattern.to, {
-                        source() {
-                            return resultSource;
-                        },
-                        size() {
-                            return resultSource.length;
-                        },
-                    });
-                }
-
-                callback();
+                compilation.updateAsset(pattern.to, {
+                    source() {
+                        return resultSource;
+                    },
+                    size() {
+                        return resultSource.length;
+                    }
+                });
             }
-        );
+
+            callback();
+        });
     }
 
     getSource(asset) {
         let source = asset.source;
-        if (typeof source === "function") {
-            source = source();
-        } else {
-            source = source.source();
-        }
+        source = typeof source === "function" ? source() : source.source();
 
-        if (source instanceof Buffer) {
-            return source.toString("utf8");
-        }
-
-        return source;
+        return source instanceof Buffer ? source.toString("utf8") : source;
     }
 }
 
