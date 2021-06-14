6cd2588d1e2c58f68f71d20ee9cf69d071e661bf
Ayfri <pierre.ayfri@gmail.com>
2021-06-14 10:12:49 +0200

    revert: Revert 316f2862e6cce35ef5627855543b01237d53493a.

--- src/default/assets/js/src/typedoc/Application.ts
+++ src/default/assets/js/src/typedoc/Application.ts
@@ -4,8 +4,8 @@ import {IComponentOptions} from './Component';
  * Component definition.
  */
 export interface IComponent {
-	constructor: new (options: IComponentOptions) => unknown;
-	selector: string;
+    constructor: new (options: IComponentOptions) => unknown;
+    selector: string;
 }
 
 /**
@@ -17,34 +17,34 @@ const components: IComponent[] = [];
  * Register a new component.
  */
 export function registerComponent(constructor: IComponent['constructor'], selector: string) {
-	components.push({
-		selector:    selector,
-		constructor: constructor
-	});
+    components.push({
+        selector:    selector,
+        constructor: constructor
+    });
 }
 
 /**
  * TypeDoc application class.
  */
 export class Application {
-	/**
-	 * Create a new Application instance.
-	 */
-	constructor() {
-		this.createComponents(document.body);
-	}
+    /**
+     * Create a new Application instance.
+     */
+    constructor() {
+        this.createComponents(document.body);
+    }
 
-	/**
-	 * Create all components beneath the given jQuery element.
-	 */
-	public createComponents(context: HTMLElement) {
-		components.forEach(c => {
-			context.querySelectorAll<HTMLElement>(c.selector).forEach(el => {
-				if (!el.dataset.hasInstance) {
-					new c.constructor({el: el});
-					el.dataset.hasInstance = String(true);
-				}
-			});
-		});
-	}
+    /**
+     * Create all components beneath the given jQuery element.
+     */
+    public createComponents(context: HTMLElement) {
+        components.forEach(c => {
+            context.querySelectorAll<HTMLElement>(c.selector).forEach(el => {
+                if (!el.dataset.hasInstance) {
+                    new c.constructor({el: el});
+                    el.dataset.hasInstance = String(true);
+                }
+            });
+        });
+    }
 }
